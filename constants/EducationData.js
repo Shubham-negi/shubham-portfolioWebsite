fc0e52044d7e1db34284ad4e40f082f260f0f9c9
Shubham negi <sbmnegi24x7@gmail.com>
2024-11-23 22:41:22 +0530

    education data
    Added the collage images

--- constants/EducationData.js
+++ constants/EducationData.js
@@ -1,23 +1,24 @@
 export const EducationData = [
     {
         name: "Bachelor of technology",
-        image:            "http://res.cloudinary.com/dhixp5pnk/image/upload/v1696219056/edekjqivqlnsyg8pfmxw.png",
+        image:"/images/UTU.png",
+
         schoolOrCollege: "Uttarakhand technical university, Dehradun",
         fromTo: "2012 - 2016",
         statusOrPrecentage: "Computer science",
     },
     {
         name: "Senior Secondary Examination",
-        image:
-            "http://res.cloudinary.com/dhixp5pnk/image/upload/v1696219649/jkjfvdlxjntnyrxs57ia.png",
+        image:"/images/UKB.png",
+
         schoolOrCollege: "Uttarakhand board",
         fromTo: "2012",
         statusOrPrecentage: "",
     },
     {
         name: "Secondary Examination",
-        image:
-            "http://res.cloudinary.com/dhixp5pnk/image/upload/v1696219649/tnverjapfr4slg7fnfcw.png",
+        image:"/images/UKB.png",
+
         schoolOrCollege: "Uttarakhand board",
         fromTo: "2009",
         statusOrPrecentage: "",
